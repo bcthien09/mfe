baa3559e70baddae0d83adee084b1ba29dc589ab
FinnB <bcthien09@gmail.com>
2025-07-14 23:23:14 +0700

    Add auth ui

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,37 @@
-import React from 'react';
-import MarketingApp from './components/MarketingApp';
+import React, {lazy, Suspense, useState} from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
+
+import Header from './components/Header';
+import Progress from './components/Progress';
+const Marketing = lazy(() => import('./components/MarketingApp'));
+const Auth = lazy(() => import('./components/AuthApp'));
+
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
 
 export default () => {
+  const [isSignedIn, setIsSignedIn] = useState(false);
   return (
-    <div>
-      <h1>Hi there123!</h1>
-      <hr />
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header onSignOut={() => setIsSignedIn(false)} isSignedIn={isSignedIn} />
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route path="/auth">
+                <Auth onSignIn={() => setIsSignedIn(true)} />
+              </Route>
+              <Route path="/" component={Marketing} />
+            </Switch>
+          </Suspense>
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
   );
 };
